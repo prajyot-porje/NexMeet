ae846646bffc004a13c373b009f135abc6ca9fa4
prajyot-porje <prajyotyt18gmail.com>
2024-12-04 22:48:40 +0530

    recoding page design

--- app/(root)/(home)/recordings/page.tsx
+++ app/(root)/(home)/recordings/page.tsx
@@ -5,6 +5,7 @@ const Recordings = () => {
     <section className='flex flex-col size-full gap-10 text-white'>
       <h1 className='text-3xl font-bold'>
         Recordings
+        
       </h1>
     </section>
   )
