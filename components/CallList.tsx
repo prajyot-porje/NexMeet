fa49e56455790b108310de3fefc2cf1041be82c3
prajyot-porje <prajyotyt18@gmail.com>
2024-12-12 21:20:26 +0530

    upcoming section [#1]

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -1,6 +1,26 @@
+'use client'
+
+import { useGetCalls } from '@/hooks/useGetCalls'
+import { useRouter } from 'next/router';
 import React from 'react'
 
-const CallList = (type:{type: 'upcoming' | 'ended' | 'recordings'}) => {
+
+const CallList = ({type}:{type: 'upcoming' | 'ended' | 'recordings'}) => {
+  const {endedCalls , upcomingCalls , callRecordings , isloading} = useGetCalls();
+  const router = useRouter();
+  const getCalls = ()=>{
+    switch (type) {
+      case 'ended' :
+        return endedCalls; 
+      case 'recordings':
+        return ;
+      case 'upcoming':
+        return upcomingCalls;
+    
+      default:
+        break;
+    }
+  }
   return (
     <div>CallList</div>
   )
